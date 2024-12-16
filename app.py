88672011aec686da6e365f9e275a87c2745fc782
Jatin Sharma <heyiamjatin001@gmail.com>
2024-12-16 11:24:38 +0530

    last commit

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ def addition(a,b):
     return a+b
 
 def division(a,b):
-<<<<<<< HEAD
-    return a/b
-=======
     return a/b
 
 def substraction(a,b):
     return a-b
->>>>>>> developer_a
 
 
 def multiply(a,b):
